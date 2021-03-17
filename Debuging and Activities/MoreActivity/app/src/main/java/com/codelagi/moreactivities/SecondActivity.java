15d6bb53bf0c4fefadd56ef5e5a0578efd5c50c3
Hendri <ndrianggar@gmail.com>
2021-03-17 14:58:55 +0700

    MoreActivities

--- Debuging and Activities/MoreActivity/app/src/main/java/com/codelagi/moreactivities/SecondActivity.java
+++ Debuging and Activities/MoreActivity/app/src/main/java/com/codelagi/moreactivities/SecondActivity.java
@@ -1,6 +1,10 @@
 package com.codelagi.moreactivities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,27 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SecondActivity extends AppCompatActivity {
 
     TextView textView;
+    EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        textView = (TextView) findViewById(R.id.textView);
+        textView = (TextView) findViewById(R.id.viewGetFirstActivity);
+        editText = findViewById(R.id.editText);
 
+        // Menangkap data yang dikirim dari page 1
         String str = getIntent().getStringExtra("username");
         textView.setText("Welcome " + str);
 
     }
 
-    public void goToHome(){}
+    public void sendBackData(View view){
+        Intent intent = new Intent();
+        intent.putExtra("databack", editText.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
 }
